f2511acddc0c75d6d0829d702a7bdfe60ecc6aa5
ucheBest <akah.uchechi@gmail.com>
2025-05-02 11:57:35 +0100

    7. Building an API

---
+++ src/main/java/com/codewithmosh/store/entities/Message.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Message {
+    private String text;
+}
